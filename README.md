600320a109a72bbaa830a006cf226797fd76f525
Piotr Gacek <piotr@gacek.dev>
2025-06-26 12:15:41 +0200

    feat: initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# grafana-alloy-remote-config
+remote server config source code: https://github.com/opsplane-services/alloy-remote-config-server
---
+++ alloy-remote-config-server/pkg/config/grpc.go
@@ -0,0 +1,82 @@
+package config
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"strings"
+
+	"golang.org/x/net/http2"
+	"golang.org/x/net/http2/h2c"
+
+	http "net/http"
+
+	connect "connectrpc.com/connect"
+	v1 "github.com/grafana/alloy-remote-config/api/gen/proto/go/collector/v1"
+	collectorv1 "github.com/grafana/alloy-remote-config/api/gen/proto/go/collector/v1/collectorv1connect"
+)
+
+type Metadata struct {
+	Id         string
+	Attributes map[string]string
+}
+
+type ImplementedCollectorServiceHandler struct{}
+
+func (ImplementedCollectorServiceHandler) GetConfig(
+	ctx context.Context,
+	req *connect.Request[v1.GetConfigRequest],
+) (*connect.Response[v1.GetConfigResponse], error) {
+	configID := req.Msg.GetId()
+	localAttributes := req.Msg.GetLocalAttributes()
+	metadata := Metadata{Id: configID, Attributes: localAttributes}
+	templateName, ok := localAttributes["template"]
+	if !ok {
+		templateName = "default"
+	}
+	tmpl, ok := templates[templateName]
+	if !ok {
+		return nil, fmt.Errorf("Template %s not found", templateName)
+	}
+
+	var resolvedConfig strings.Builder
+	err := tmpl.Execute(&resolvedConfig, metadata)
+	if err != nil {
+		return nil, err
+	}
+
+	globalStorage.Set(configID, resolvedConfig.String())
+	res := connect.NewResponse(&v1.GetConfigResponse{Content: resolvedConfig.String()})
+	return res, nil
+}
+
+func (ImplementedCollectorServiceHandler) RegisterCollector(
+	ctx context.Context,
+	req *connect.Request[v1.RegisterCollectorRequest],
+) (*connect.Response[v1.RegisterCollectorResponse], error) {
+	configID := req.Msg.GetId()
+	log.Printf("Register: %v [not used - agents are registered by getConfig call]", configID)
+	res := connect.NewResponse(&v1.RegisterCollectorResponse{})
+	return res, nil
+}
+
+func (ImplementedCollectorServiceHandler) UnregisterCollector(
+	ctx context.Context,
+	req *connect.Request[v1.UnregisterCollectorRequest],
+) (*connect.Response[v1.UnregisterCollectorResponse], error) {
+	configID := req.Msg.GetId()
+	log.Printf("Unregister: %v [not used - agents are unregistered once not accessed for long time]", configID)
+	res := connect.NewResponse(&v1.UnregisterCollectorResponse{})
+	return res, nil
+}
+
+func StartConnectGrpcServer(port int) {
+	mux := http.NewServeMux()
+	mux.Handle(collectorv1.NewCollectorServiceHandler(&ImplementedCollectorServiceHandler{}))
+	log.Printf("Start listening (gRPC) on port %d", port)
+	err := http.ListenAndServe(
+		fmt.Sprintf("0.0.0.0:%d", port),
+		h2c.NewHandler(mux, &http2.Server{}),
+	)
+	log.Fatalf("listen failed: %v", err)
+}
